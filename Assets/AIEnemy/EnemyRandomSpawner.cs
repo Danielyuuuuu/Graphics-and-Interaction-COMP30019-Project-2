0c9daeef8ed464fc3ea5980d030a202b6dec7552
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-06 22:48:39 +1100

    The enemy can now spawn within a given time frame

--- Assets/AIEnemy/EnemyRandomSpawner.cs
+++ Assets/AIEnemy/EnemyRandomSpawner.cs
@@ -6,12 +6,35 @@ public class EnemyRandomSpawner : MonoBehaviour
 {
   public Transform[] spawnPoints;
   public GameObject[] enemyPrefabs;
+  private bool stopSpawning = false;
+  private float spawnTime = 2f;
+  private float spawnDelay = 0.5f;
+  private bool firstSpawnDone = false;
+  private float firstSpawnTime;
+  private float totalSpawnTime = 10f;
 
   public void Start()
   {
-
+    InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
   }
 
+  public void SpawnObject()
+  {
+    if (!firstSpawnDone)
+    {
+      firstSpawnTime = Time.time;
+      firstSpawnDone = true;
+    }
+
+    int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+    Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
+    if ((Time.time - firstSpawnTime) > totalSpawnTime)
+    {
+      CancelInvoke("SpawnObject");
+    }
+  }
+
+  /*
   private void Update()
   {
     if (Input.GetMouseButtonDown(0))
@@ -22,4 +45,6 @@ public class EnemyRandomSpawner : MonoBehaviour
       Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
     }
   }
+    */
+
 }
