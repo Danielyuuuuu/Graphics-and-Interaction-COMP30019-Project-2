d8a13b2b8ea483583fbb0b8b7c4a094c8f0770fd
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-10-09 18:29:14 +1100

    add ShootingEnemy scene

---
+++ Assets/ShootingEnemyFolder/DestroyIfOffscreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIfOffscreen : MonoBehaviour
+{
+    // Triggered as soon as the object is outside of the camera frustrum
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
