3607bb64d3d1d22b54d8d65efbff3c431dddfd7a
Adrian <chihout@student.unimelb.edu.au>
2020-10-10 18:37:37 +1100

    basic topdown camera view following player movement

---
+++ Assets/Camera Demo/CameraControlDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlDemo : MonoBehaviour
+{
+    public Transform target;
+    public int YAxisAway = 30;
+    public int ZAxisAway = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerLocation = target.position;
+        this.transform.position = new Vector3(playerLocation.x, playerLocation.y+YAxisAway, playerLocation.z+ZAxisAway);
+        // this.transform.LookAt(target);
+    }
+}
---
+++ Assets/Camera Demo/PlayerControllerDemo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float moveSpeed = 10f;
+    Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WASDMovement();
+        Turning();
+    }
+
+    void WASDMovement()
+    {
+        Vector3 currentPosition = this.transform.position;
+
+        // Move in x and z direction
+        /* Camera movement should NOT be relative to where it is facing. */
+        if (Input.GetKey("w"))
+        {
+            currentPosition += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            currentPosition += Vector3.forward * -moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            currentPosition += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            currentPosition += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        // Apply the new position
+        this.transform.position = currentPosition;
+    }
+
+    /**
+     * Using Raycase has a problem.
+     * If mouse cursor move outside of the map, no detection found.
+     * Thus cant turn around.
+     * Need to come up a new solution in the future.
+     */
+    void Turning ()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    
+        if(Physics.Raycast(ray,out hit,100))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+
+}
