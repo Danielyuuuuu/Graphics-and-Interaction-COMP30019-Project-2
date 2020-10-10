93ffdca29c7c7442e29290e17d3e98365363c4ac
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-10 22:16:53 +1100

    Keep track of the number of enemies in the game

--- Assets/AIEnemy/EnemyRandomSpawner.cs
+++ Assets/AIEnemy/EnemyRandomSpawner.cs
@@ -7,17 +7,19 @@ public class EnemyRandomSpawner : MonoBehaviour
   public Transform[] spawnPoints;
   public GameObject[] enemyPrefabs;
   private bool stopSpawning = false;
-  public float spawnTime = 2f;
+  public float startSpawnTime = 2f;
   public float spawnDelay = 0.5f;
   private bool firstSpawnDone = false;
   private float firstSpawnTime;
   public float totalSpawnTime = 1000f;
 
   private int startChildCount;
+  private int currentEnemyCount = 0;
+  private int numOfEnemySpawned = 0;
 
   public void Start()
   {
-    InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    InvokeRepeating("SpawnObject", startSpawnTime, spawnDelay);
     startChildCount = this.transform.childCount;
   }
 
@@ -32,6 +34,9 @@ public class EnemyRandomSpawner : MonoBehaviour
     int randSpawnPoint = Random.Range(0, spawnPoints.Length);
     GameObject enemy = Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
     enemy.transform.parent = this.transform;
+
+    numOfEnemySpawned++;
+
     if ((Time.time - firstSpawnTime) > totalSpawnTime)
     {
       CancelInvoke("SpawnObject");
@@ -40,20 +45,9 @@ public class EnemyRandomSpawner : MonoBehaviour
 
   private void Update()
   {
-    Debug.Log(this.transform.childCount - startChildCount);
+    currentEnemyCount = this.transform.childCount - startChildCount;
+    Debug.Log("Current enemy count: " + currentEnemyCount);
+    Debug.Log("numOfEnemySpawned: " + numOfEnemySpawned);
   }
-
-  /*
-  private void Update()
-  {
-    if (Input.GetMouseButtonDown(0))
-    {
-      int randEnemy = Random.Range(0, enemyPrefabs.Length);
-      int randSpawnPoint = Random.Range(0, spawnPoints.Length);
-
-      Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
-    }
-  }
-    */
 
 }
