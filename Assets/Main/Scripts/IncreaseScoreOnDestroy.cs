f194a39aaf2c2a955d73f00fead31f7b67602b0d
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-14 18:11:47 +1100

    Implemented the game score

---
+++ Assets/Main/Scripts/IncreaseScoreOnDestroy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseScoreOnDestroy : MonoBehaviour
+{
+
+  public int incrementAmount;
+  public ScoreManager scoreManager;
+
+
+  void Start()
+  {
+    // Find score manager by tag, if not referenced already
+    if (scoreManager == null)
+    {
+      this.scoreManager = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
+    }
+  }
+
+  // Increment player score when destroyed
+  void OnDestroy()
+  {
+    Debug.Log("Increase score: " + this.incrementAmount);
+    this.scoreManager.score += this.incrementAmount;
+  }
+}
