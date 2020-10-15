3c0ae2c2d907bdda2c914b34730aa54bbe832755
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-15 14:13:59 +1100

    The shop item can now be displayed in the game

--- Assets/Main/Scripts/UIShop.cs
+++ Assets/Main/Scripts/UIShop.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIShop : MonoBehaviour
 {
@@ -11,6 +13,26 @@ public class UIShop : MonoBehaviour
   {
     container = transform.Find("container");
     shopItemTemplate = container.Find("shopItemTemplate");
-    shopItemTemplate.gameObject.SetActive(false);
+    //shopItemTemplate.gameObject.SetActive(false);
+  }
+
+  private void Start()
+  {
+    CreateItemButton("Item 1", 15, 0);
+    CreateItemButton("Item 2", 30, 1);
+  }
+
+  private void CreateItemButton(string itemName, int itemCost, int positionIndex)
+  {
+    Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+    RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+    float shopItemHeight = 70f;
+    shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+    shopItemTransform.Find("itemNameText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+    shopItemTransform.Find("itemCostText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+    shopItemTransform.gameObject.SetActive(true);
   }
 }
