8645b5e80402d4c542e761112d38e6ae6f9c44e9
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-15 18:57:29 +1100

    Added the store click event

--- Assets/Main/Scripts/UIShop.cs
+++ Assets/Main/Scripts/UIShop.cs
@@ -12,7 +12,7 @@ public class UIShop : MonoBehaviour
   private void Awake()
   {
     container = transform.Find("container");
-    shopItemTemplate = container.Find("shopItemTemplate");
+    shopItemTemplate = container.Find("test");
     //shopItemTemplate.gameObject.SetActive(false);
   }
 
@@ -24,6 +24,7 @@ public class UIShop : MonoBehaviour
     Hide();
   }
 
+  /*
   private void CreateItemButton(string itemName, int itemCost, int positionIndex)
   {
     Transform shopItemTransform = Instantiate(shopItemTemplate, container);
@@ -36,6 +37,28 @@ public class UIShop : MonoBehaviour
     shopItemTransform.Find("itemCostText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
 
     shopItemTransform.gameObject.SetActive(true);
+  }
+  */
+  private void CreateItemButton(string itemName, int itemCost, int positionIndex)
+  {
+    Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+    RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+    float shopItemHeight = 70f;
+    shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+    shopItemTransform.Find("Button").Find("itemNameText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+    shopItemTransform.Find("Button").Find("itemCostText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+    Button btn = shopItemTransform.Find("Button").GetComponent<Button>();
+    btn.onClick.AddListener(TaskOnClick);
+
+    shopItemTransform.gameObject.SetActive(true);
+  }
+
+  void TaskOnClick()
+  {
+    Debug.Log("You have clicked the button!");
   }
 
   public void Show(GameObject player)
