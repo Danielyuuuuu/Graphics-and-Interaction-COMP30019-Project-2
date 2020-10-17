7107a346bda6dae3923955f6c6d4b02d25d94656
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-10-18 02:46:17 +1100

    add collision detection for firebullet particle system

---
+++ Assets/ShootingEnemyFolder/ParticleCollideWith.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollideWith : MonoBehaviour
+{
+    public string tagToDamage;
+    public int damageAmount = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other) {
+        
+        if (other.tag == tagToDamage)
+        {
+            // Damage object with relevant tag
+            HealthManager healthManager = other.GetComponent<HealthManager>();
+            healthManager.ApplyDamage(damageAmount);
+
+            // Destroy self
+            //Destroy(this.gameObject);
+
+            Debug.Log("Particle collide with!!");
+            Debug.Log(other.tag);
+        }
+    }
+}
