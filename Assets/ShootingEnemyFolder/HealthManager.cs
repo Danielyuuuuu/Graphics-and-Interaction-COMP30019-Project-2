78bd865119a8021972a0a36c8ae022fd9c179d76
Adrian <chihout@student.unimelb.edu.au>
2020-10-28 04:35:54 +1100

    damage layer

--- Assets/ShootingEnemyFolder/HealthManager.cs
+++ Assets/ShootingEnemyFolder/HealthManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class HealthManager : MonoBehaviour
 {
@@ -10,12 +12,14 @@ public class HealthManager : MonoBehaviour
 
     public GameObject destroyExplosionPrefab;
     public GameObject destroyExplosionPrefab_Rocket;
+    public Image damageScreen;
 
     public UnityEvent zeroHealthEvent;
 
     Animator animator;
+    float alpha;
 
-  void Start()
+    void Start()
     {
         this.ResetHealthToStarting();
         animator = GetComponent<Animator>();
@@ -32,42 +36,47 @@ public class HealthManager : MonoBehaviour
     public void ApplyDamage(int damage, string tag)
     {
         if (currentHealth > 0)
-      {
-        // Trigger Hit animation
-        DetectHit();
-        currentHealth -= damage;
-
-        if (currentHealth <= 0)
         {
-            if (this.tag != "Player")
+            // Trigger Hit animation
+            DetectHit();
+            currentHealth -= damage;
+            
+            if (this.tag == "Player")
             {
+                // Screen should flash red when player got hit
+                StopCoroutine( FadeOut() );
+                damageScreen.color = new Color(1f, 1f, 1f, 1f);
+                StartCoroutine( FadeOut() );
+            }
+
+            // this hit would kill the current game object
+            if (currentHealth <= 0)
+            {
+                if (this.tag != "Player")
+                {
                     if (tag == "Bullet")
                     {
                         GameObject explosion = Instantiate(this.destroyExplosionPrefab);
                         explosion.transform.position = this.transform.position;
                     }
                     else if (tag == "Rocket") {
-                    //else { 
                         GameObject explosion = Instantiate(this.destroyExplosionPrefab_Rocket);
                         explosion.transform.position = this.transform.position;
                     }
                     
 
                     Destroy(this.gameObject);
-            }
-            else
-            {
-              animator.SetTrigger("Die");
-              this.zeroHealthEvent.Invoke();
-
-              GameObject weapon = GameObject.FindGameObjectWithTag("Weapon");
-              weapon.SetActive(false);
-
-
-
                 }
-      }
-      }
+                else
+                {
+                    animator.SetTrigger("Die");
+                    this.zeroHealthEvent.Invoke();
+
+                    GameObject weapon = GameObject.FindGameObjectWithTag("Weapon");
+                    weapon.SetActive(false);
+                }
+            }
+        }
     }
 
     // Get the current health of the object
@@ -90,4 +99,22 @@ public class HealthManager : MonoBehaviour
         animator.ResetTrigger("Hit");
         animator.SetTrigger("Hit");
     }
+
+    IEnumerator FadeOut()
+    {
+        float timePassed = 0f;
+        // 1 is the fading duration (how long it takes to fade)
+        while (timePassed < 0.5)
+        {
+            var lerpFactor = timePassed / 0.5;
+            damageScreen.color = new Color (1f, 1f, 1f, Mathf.Lerp(1f, 0f, (float)lerpFactor));
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        // make sure in the end the damage layer disappear
+        damageScreen.color = new Color (1f, 1f, 1f, 0f);
+    }
+
+
 }
