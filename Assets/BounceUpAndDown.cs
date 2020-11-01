87a3b7f706c50a154a6f2023a4a673022c5a8f2e
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-11-01 18:07:54 +1100

    Added bouncing shop icon

---
+++ Assets/BounceUpAndDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceUpAndDown : MonoBehaviour
+{
+
+  public AnimationCurve animationCurve;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.position = new Vector3(transform.position.x, animationCurve.Evaluate((Time.time * 1.35f % animationCurve.length)) + 4.0f, transform.position.z);
+    }
+}
