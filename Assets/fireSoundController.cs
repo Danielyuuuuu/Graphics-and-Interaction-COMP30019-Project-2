c49fbb32bc04c811af4da69fd9153bb249c1446a
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-11-02 01:44:41 +1100

    fix fire explosion sound can't be played completely

---
+++ Assets/fireSoundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireSoundController : MonoBehaviour
+{
+    AudioSource fireAudio;
+    public AudioClip fireSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireAudio = GetComponent<AudioSource>();
+        fireAudio.PlayOneShot(fireSound);
+    }
+
+    
+}
